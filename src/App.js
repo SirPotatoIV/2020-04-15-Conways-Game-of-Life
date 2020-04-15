da2a0b336ec25c43e8717f042122aab99f0dd346
SirPotatoIV <otoolej33@gmail.com>
2020-04-15 12:01:43 -0500

    Created new file to start building game logic. Tested by adding to App.js so I could check for a console log.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import gridCreator from "./game-logic/grid";
 
 function App() {
+  gridCreator();
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/game-logic/grid.js
@@ -0,0 +1,4 @@
+export default function gridCreator() {
+  console.log("test");
+}
+gridCreator();
